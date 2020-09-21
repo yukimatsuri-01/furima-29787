74e622bc259ded3a9e8a12907a54d2c6e7007c9e
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-21 12:54:21 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_kanji, :first_name_kanji, :family_name_katakana, :first_name_katakana, :birth_day])
+  end
 
   private
 
