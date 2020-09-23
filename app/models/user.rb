82533daa39f28a0b926948edd4fc8f4474424201
yukimatsuri-01 <70310758+yukimatsuri-01@users.noreply.github.com>
2020-09-23 16:16:42 +0900

    user.rbの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     validates :first_name_kanji, format: { with: /\A[一-龥ぁ-ん]+\z/ }
     validates :family_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
   end
-  validates :birth_day, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
 end
