c310687a37baa5779faeb2580dd15f1bc92065db
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-23 15:39:58 +0900

    Userモデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 40 }
+  with_options presence: true do
+    validates :family_name_kanji, format: { with: /\A[一-龥ぁ-ん]+\z/ }
+    validates :first_name_kanji, format: { with: /\A[一-龥ぁ-ん]+\z/ }
+    validates :family_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
+  validates :birth_day, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i }
 end
