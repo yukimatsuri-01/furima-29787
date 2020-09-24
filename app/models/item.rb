b09b12ecbe8a074c4b230d37b488a2967f6ff7f2
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-24 18:25:15 +0900

    categoryモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,24 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :sale_price, format: { with: /\A[0-9]+\z/ }, numericality: {greater_than: 300, less_than: 10000000
+    }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :bear_shipping_fee_id
+    validates :shipping_from_id
+    validates :shipping_within_days_id
+  end
 end
