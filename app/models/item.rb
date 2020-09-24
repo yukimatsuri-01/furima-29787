e85bdc577afd45a930fe992248aaaae8be56dff3
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-24 19:09:53 +0900

    shipping_fromモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :bear_shipping_fee
-
+  belongs_to_active_hash :shipping_from
 
   with_options presence: true do
     validates :image
