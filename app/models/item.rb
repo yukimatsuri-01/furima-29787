acf3e8c0c6b40d82285c47910aedc135f90c847e
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-24 18:47:29 +0900

    conditionモデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
 
 
   with_options presence: true do
