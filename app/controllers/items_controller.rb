bc91aae658e05537717d14ee5e3e7b916585cb5e
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-24 12:47:36 +0900

    商品出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :bear_shipping_fee_id, :shipping_from_id, :shipping_within_days_id, :sale_price).merge(user_id: current_user.id)
+  end
+
 end
