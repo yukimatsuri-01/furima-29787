b8b85f309a0e85fc2e80f329fcc62c6aa2362b5a
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-09-28 17:21:11 +0900

    商品出品時に登録した情報を見る実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to item_path(@item)
     else
       render :new
     end
