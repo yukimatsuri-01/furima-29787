306a7fa68c924685b1c227a8cad02305a6bf0ae6
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-01 18:05:04 +0900

    ordersルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items
+  resources :items do
+    resources :orders, only: [:index]
+      collection do
+        get 'done', to: 'orders#done'
+        post 'pay', to: 'orders#pay'
+      end
+    end
+  end
 end
