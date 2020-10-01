cdf820c50cb52528d97ee5a69c10e62afe9a7f1c
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-01 18:39:34 +0900

    addressesテーブルの作成

--- db/migrate/20201001092756_create_addresses.rb
+++ db/migrate/20201001092756_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :postal_code,    null: false
+      t.integer :prefecture_id, null: false
+      t.string :city,           null: false
+      t.string :address_line,   null: false
+      t.string :building
+      t.string :phone_number,   null: false
+      t.references :order,      null: false, foreing_key: true
       t.timestamps
     end
   end
