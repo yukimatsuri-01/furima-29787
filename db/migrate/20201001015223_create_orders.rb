dcaba0d321c6ab803ee32d066cfa7073c1068a5b
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-01 10:57:25 +0900

    ordersテーブルの作成

--- db/migrate/20201001015223_create_orders.rb
+++ db/migrate/20201001015223_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.references :user, null: false, foreing_key: true
+      t.references :item, null: false, foreing_key: true
       t.timestamps
     end
   end
