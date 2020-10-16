d31324b80883b966f9d2b9f216b8df67a4b60a7c
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-16 11:17:41 +0900

    Addressモデル編集

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,14 @@
 class Address < ApplicationRecord
-  belongs_to :order 
-
+  belongs_to :user, optional: true 
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture_id, numericality: { other_than: 1}
+    validates :city
+    validates :address_line
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/}
+  end
 end
