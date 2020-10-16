917088545e328b83109c16c34d08aba378fd70e8
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-16 15:57:46 +0900

    新規登録機能の実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,39 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(sign_up_params)
+      unless @user.valid?
+        render :new and return
+      end
+    session["divise.regist_data"] = {user: @user.attributes}
+    session["divise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+
+  def create_address
+    @user = User.new(session["divise.regist_data"] ["user"])
+    @address = Address.new(address_params)
+      unless @address.valid?
+        render :new_address
+      end
+    @user.build_address(@address.attributes)
+    @user.save
+    session["divise.regist_data"]["user"].clear
+    sign_in(:user, @user)  
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :prefecture_id, :city, :address_line, :building, :phone_number)
+  end
+  
   # GET /resource/sign_up
   # def new
   #   super
