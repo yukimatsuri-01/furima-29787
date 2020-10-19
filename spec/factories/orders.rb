b2446e319e40ebf8a222c1e0c6a0516797bca40b
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-19 19:33:54 +0900

    orderテスコード作成

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :order do
-
+    token {'tok_abcdefghijk00000000000000000'}
+    association :item
+    association :user
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入情報の保存' do
+    before do
+      @order = FactoryBot.build(:order)
+    end
+
+    context '商品が保存できる場合' do
+      it '全ての値が正しく入力されていれば登録できること' do
+        expect(@order).to be_valid
+      end
+    end
+
+    context '商品が保存できない場合' do
+      it 'tokenが空では保存できない' do
+        @order.token = nil
+        @order.valid?
+        expect(@order.errors.full_messages).to include("Tokenを入力してください")
+     end
+    end 
+  end    
 end
