9c53f6699eee785aff33611711ecaa6731810dae
yukimatsuri-01 <matsunariyukiko@gmail.com>
2020-10-20 11:00:00 +0900

    20201014023427_add_id_to_addresses.rb修正

--- db/migrate/20201014023427_add_id_to_addresses.rb
+++ db/migrate/20201014023427_add_id_to_addresses.rb
@@ -2,7 +2,7 @@ class AddIdToAddresses < ActiveRecord::Migration[6.0]
   def change
     add_reference :addresses, :user
     remove_reference :addresses, :order
-    Address.create(postal_code: 874-0044, prefecture_id: 45, city: "別府", address_line: 1-1, building: "beppuビル", phone_number: 09011112222, user_id: 1)
+    Address.create(postal_code: 874-0044, prefecture_id: 45, city: "別府", address_line: 1-1, building: "beppuビル", phone_number: 09011112222)
     Address_reset_column_information
   end
 end
